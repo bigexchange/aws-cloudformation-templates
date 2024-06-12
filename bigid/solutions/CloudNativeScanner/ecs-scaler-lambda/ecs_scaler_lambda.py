9cd1029b61e24a5762049205becc1e0fac5decf9
Gregory Kalmring <gkalmring@bigid.com>
2024-06-12 17:01:52 -0400

    Add Truthy Statements for Validation

--- bigid/solutions/CloudNativeScanner/ecs-scaler-lambda/ecs_scaler_lambda.py
+++ bigid/solutions/CloudNativeScanner/ecs-scaler-lambda/ecs_scaler_lambda.py
@@ -125,6 +125,7 @@ def main(
     if system_token:
         jobs = get_scans_jobs(hostname, system_token,scanner_group)
         scanners = get_scanner_list(system_token,hostname, scanner_group)
+        scale = False
         print(f"jobs: {jobs}")
         print(f"scanners:{len(scanners)}")
         print(f"min:{minimum_desired_count}")
@@ -134,18 +135,23 @@ def main(
         # If there are more active scanners than needed, scale down the scanner count.
         if not jobs and len(scanners) > minimum_desired_count:
             desired_count = minimum_desired_count
+            scale = True
             print("Scaling Down")
         elif jobs and len(scanners) < int(desired_count):
             desired_count = desired_count
+            scale = True
             print("Scaling up Scanners")
-        # Get scans and scale ECS task definition based on the result
-        result = scale_ecs_task_definition(
-            cluster_name,
-            service_name,
-            desired_count,
-            region_name,
-        )
-        return result
+        if scale:
+            #  Get scans and scale ECS task definition based on the result
+            print("Calling ECS Task Definition")
+            result = scale_ecs_task_definition(
+                cluster_name,
+                service_name,
+                desired_count,
+                region_name,
+            )
+            return result
+        return None
     else:
         return None
 
